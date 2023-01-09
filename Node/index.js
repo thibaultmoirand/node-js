8a904cebe29981e6df8487da2c8f3efe7900694b
thibault <thibault.moirand@loatys.com>
2023-01-09 09:03:46 +0100

    petit retard

--- Node/index.js
+++ Node/index.js
@@ -1,17 +1,76 @@
+//tp 1
+const fs = require('fs');
+fs.readFile('fichier_text.txt', 'utf8', (err, data) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  //lire le contenue du fichier
+  console.log(data);
+});
+
+
+
+//tp 2
+
 const fs = require('fs');
 
 const content = 'la la la';
+//le text a ecrire
 
 fs.writeFile('fichier_text.txt', content, err => {
   if (err) {
     console.error(err);
   }
-  // fichier écrit avec succès
+// pour ecrire
 });
 fs.readFile('fichier_text.txt', 'utf8', (err, data) => {
     if (err) {
       console.error(err);
       return;
     }
+    // pour lire le resulta
     console.log(data);
   });
+
+  //tp 4
+  
+const readDir = fs.readdirSync('/test',(anError, anData)=>{ 
+  // set quelle repertoir examiner 
+  if (anError) {
+      console.log(anError);
+      return
+  }
+
+  console.log(anData);
+  // affichage du resulta du repertoir examiner
+})
+
+console.log(readDir);
+
+//tp 5
+const fs = require("fs")
+
+fs.appendFile("fichier.txt", "la la la",(err) => { 
+  //appedFile crrée le ficher et ecris dedant
+    if (err) throw err;
+    console.log('création fini');
+  });
+
+  //tp 6
+  const os = require ("os")
+
+console.log(os.homedir()) // affiche home de l'utilisateur
+
+//tp 7
+const exec = require('child_process').exec;  
+exec('ipconfig', (err, stdout, stderr) => { 
+  if (err) {  
+    console.error(err);  
+    return;  
+  }  
+  console.log(stdout);  
+});  
+
+//tp 8
+const path = require('path');
--- Node/server.js
+++ Node/server.js
@@ -1,19 +1,27 @@
+
+//tp server (3)
+
+
 const http = require('http');
 const fs = require('fs').promises;
 
 const host = 'localhost';
 const port = 8080;
 
-const requestListener = function (req, res) { //on def la requete
-    fs.readFile(__dirname + "/index.html") //permet de lire un fichier dans un seveur
+const requestListener = function (req, res) { 
+    //la requete
+    fs.readFile(__dirname + "/index.html")
     .then(contents => {
-        res.setHeader("Content-Type", "text/html"); //on definit le type de fichier à ouvrir
+        res.setHeader("Content-Type", "text/html"); 
+        //quelle type de fichier ouvrir
         res.writeHead(200);
         res.end(contents);
     })
 };
 
-const server = http.createServer(requestListener);  //on crée le serveur 
+const server = http.createServer(requestListener);  
+//on crée le server http
 server.listen(port, host, () => {
-    console.log(`Server is running on http://${host}:${port}`); //on affiche si le serveur est en marche
+    console.log(`Server is running on http://${host}:${port}`); 
+    //affichage du resulta
 });
